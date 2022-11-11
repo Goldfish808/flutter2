a163e9770c9dfd3cfeb205f0defbbd62e797a71b
Goldfish808 <tjddns0853@gmail.com>
2022-11-11 17:24:01 +0900

    이 시점에서 scroll view 가 가운데로 배치되었음

--- lib/screens/chat_room_screen.dart
+++ lib/screens/chat_room_screen.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_katalk/components/my_chat.dart';
+import 'package:flutter_katalk/components/ohter_chat.dart';
 import 'package:flutter_katalk/components/time_line.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class ChatRoomScreen extends StatefulWidget {
-  const ChatRoomScreen({Key? key}) : super(key: key);
-
   @override
-  State<ChatRoomScreen> createState() => _ChatRoomScreenState();
+  _ChatRoomScreenState createState() => _ChatRoomScreenState();
 }
 
 class _ChatRoomScreenState extends State<ChatRoomScreen> {
+  final List<MyChat> chats = [];
+  final TextEditingController _textController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -18,7 +21,10 @@ class _ChatRoomScreenState extends State<ChatRoomScreen> {
         backgroundColor: Colors.transparent,
         appBar: AppBar(
           backgroundColor: Colors.transparent,
-          title: Text("홍길동동동주", style: Theme.of(context).textTheme.headline6),
+          title: Text(
+            "홍길동",
+            style: Theme.of(context).textTheme.headline6,
+          ),
           actions: [
             Icon(FontAwesomeIcons.search, size: 20),
             SizedBox(width: 25),
@@ -30,12 +36,23 @@ class _ChatRoomScreenState extends State<ChatRoomScreen> {
           children: [
             Expanded(
               child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    //나중에 위치가 설정되어야 함
-                    TimeLine(time: "2021년 1월 1일 금요일 "),
-                    Text("sefesfsef")
-                  ],
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Column(
+                    children: [
+                      TimeLine(time: "2021년 1월 1일 금요일"),
+                      OtherChat(
+                        name: "홍길동",
+                        text: "새해 복 많이 받으세요.",
+                        time: "오전 10:10",
+                      ),
+                      MyChat(
+                        text: "선생님도 많이 받으십시오.",
+                        time: "오후 2:15",
+                      ),
+                      ...List.generate(chats.length, (index) => chats[index]),
+                    ],
+                  ),
                 ),
               ),
             ),
